4f62e00417bda0d333e38d02662786364160b103
carrotflakes <carrotflakes@gmail.com>
2023-09-24 05:10:16 +0900

    API URL from query string

--- front/src/config.ts
+++ front/src/config.ts
@@ -1,1 +1,4 @@
-export const apiUrl = import.meta.env.VITE_API_URL ?? 'http://localhost:8000/';
+export const apiUrl =
+  location.search.match(/\?api=([^&]+)/)?.[1] ??
+  import.meta.env.VITE_API_URL ??
+  "http://localhost:8000/";
